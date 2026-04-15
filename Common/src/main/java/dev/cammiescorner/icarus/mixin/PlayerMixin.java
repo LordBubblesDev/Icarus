116c9c2300f58cc426d79b1a13406bbfd968e4d8
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2026-04-15 07:40:27 +0200

    Add slighter wing movement when slow falling

--- Common/src/main/java/dev/cammiescorner/icarus/mixin/PlayerMixin.java
+++ Common/src/main/java/dev/cammiescorner/icarus/mixin/PlayerMixin.java
@@ -1,12 +1,16 @@
 package dev.cammiescorner.icarus.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.cammiescorner.icarus.api.SlowFallingEntity;
+import dev.cammiescorner.icarus.util.IcarusHelper;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity implements SlowFallingEntity {
@@ -27,5 +31,13 @@ public abstract class PlayerMixin extends LivingEntity implements SlowFallingEnt
     @Override
     public boolean icarus$isSlowFalling() {
         return icarus$slowFalling;
+    }
+
+    @ModifyReturnValue(method = "getDesiredPose", at = @At("RETURN"))
+    private Pose icarus$forceCrouchPoseWhileSlowFalling(Pose original) {
+        if (original == Pose.STANDING && icarus$slowFalling && IcarusHelper.hasWings((Player) (Object) this)) {
+            return Pose.CROUCHING;
+        }
+        return original;
     }
 }
